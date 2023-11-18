3a8efa1f789b02bf4f8501e15ea4d873774f6025
MarcuusFerreira <marcusvcf04@gmail.com>
2023-11-17 21:29:49 -0300

    :construction: Criação da procura por nome

--- src/main/java/com/senac/tcs/condominio/reserva/model/repository/CondomRepository.java
+++ src/main/java/com/senac/tcs/condominio/reserva/model/repository/CondomRepository.java
@@ -1,8 +1,10 @@
 package com.senac.tcs.condominio.reserva.model.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.senac.tcs.condominio.reserva.model.entities.Condom;
 
 public interface CondomRepository extends JpaRepository<Condom, Long> {
+    List<Condom> findByName(String name);
 } 
